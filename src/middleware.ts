0b09cfb5ced8ef7a167f00f0dcaa13cf594973e4
Nico <nicoconstantin11@gmail.com>
2024-08-10 15:31:15 +0200

    feat: add internationalization config for nextjs14

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import createMiddleware from 'next-intl/middleware';
+import {locales} from './locales'
+ 
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: locales,
+ 
+  // Used when no locale matches
+  defaultLocale: 'en'
+});
+export const config = {
+  // Match only internationalized pathnames
+  matcher: ['/', '/(en|es|fr)/:path*']
+};
+
+//Unable to do this because of Unsupported template literal with expressions at "config.matcher[1]".
+// const localesMatcher = locales.join('|')
